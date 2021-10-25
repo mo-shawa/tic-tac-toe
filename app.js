25cd40f6364ee13bb786825e744bbd11df2e24fc
Mo Shawa <moshawa0@gmail.com>
2021-10-25 19:58:00 -0400

    adds reset button functionality

--- app.js
+++ app.js
@@ -5,10 +5,25 @@
 const squares = document.querySelectorAll("main > div");
 const main = document.querySelector("main");
 let nextPlayer = "O";
+const resetBtn = document.getElementById("reset");
+
+function init() {
+	squares.forEach(function (e) {
+		e.innerHTML = "";
+	});
+}
 
 main.addEventListener("click", function (evt) {
 	const square = evt.target;
+	//
+	if (square.innerHTML) {
+		return;
+	} else {
+		square.innerHTML = `<h1>${nextPlayer}</h1>`;
+	}
 	console.log(square);
+	// Set next player
 	nextPlayer == "O" ? (nextPlayer = "X") : (nextPlayer = "O");
-	square.innerHTML = `<h1>${nextPlayer}</h1>`;
 });
+
+resetBtn.addEventListener("click", init);
