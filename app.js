090b58e46ed78db1eaf5353112e02161e823ccd5
Mo Shawa <moshawa0@gmail.com>
2021-10-27 00:38:51 -0400

    branch test

--- app.js
+++ app.js
@@ -1,29 +1,68 @@
+// // define required constants
+
+const winCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
+
+// //Define required variables to track state
+
 // define required constants
+const squares = document.querySelectorAll(".cell");
+const main = document.querySelector("main");
+
+let nextPlayer = "X";
+const resetBtn = document.getElementById("reset");
+const result = document.getElementById("result");
 
 // define required variables used to track state of the game
-
-const squares = document.querySelectorAll("main > div");
-const main = document.querySelector("main");
-let nextPlayer = "O";
-const resetBtn = document.getElementById("reset");
+let won = false;
 
 function init() {
-	squares.forEach(function (e) {
-		e.innerHTML = "";
+	won = false;
+	squares.forEach(function (evt) {
+		evt.innerHTML = "";
+		result.textContent = "X's turn";
+		nextPlayer = "X";
 	});
 }
 
 main.addEventListener("click", function (evt) {
 	const square = evt.target;
-	//
-	if (square.innerHTML) {
+	// Exit if square already played or there's a winner
+	if (square.textContent || won == true) {
 		return;
-	} else {
-		square.innerHTML = `<h1>${nextPlayer}</h1>`;
 	}
-	console.log(square);
-	// Set next player
+	// If clicked block is empty, insert X or O
+	square.innerHTML ? undefined : (square.innerHTML = `<h1>${nextPlayer}</h1>`);
+	// Check win
+	checkWin(nextPlayer)
+		? (result.textContent = `${nextPlayer} won bruh`)
+		: undefined;
+
+	// Update text
 	nextPlayer == "O" ? (nextPlayer = "X") : (nextPlayer = "O");
+	result.textContent = `${nextPlayer}'s turn`;
 });
 
-resetBtn.addEventListener("click", init);
+function checkWin(player) {
+	won = false;
+	for (let i = 0; i < winCombos.length; i++) {
+		if (
+			squares[winCombos[i][0]].textContent == player &&
+			squares[winCombos[i][1]].textContent == player &&
+			squares[winCombos[i][2]].textContent == player
+		) {
+			won = true;
+			console.log("itwork");
+		}
+	}
+	return won;
+}
+// resetBtn.addEventListener("click", init);
